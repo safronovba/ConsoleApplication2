885dd0c860ac930e7309fbec4b874970f71f8504
safronovba <safronovba@yandex.ru>
2013-04-15 08:59:23 +0700

    test

---
+++ ConsoleApplication2/box.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class BoxException : Exception
+    {
+
+        public BoxException(string message):base (message)
+        {
+           
+        } 
+
+    }
+    public class Box
+    {
+        private int width;
+        private int height;
+        private int depth;
+
+        
+        public Box(int width, int height, int depth)
+        {
+            if (width < 0) 
+            { 
+                throw new BoxException("width must be positive"); 
+            }
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Box({0}, {1}, {2}, {3})", width, height, depth, Volume);
+        }
+
+        public int Width 
+        {
+            get { return width; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+        }
+
+        public int Depth
+        {
+            get { return depth; }
+        }
+
+        public int Volume
+        {
+            get { return width * height * depth; }
+        }
+
+        public int Volume2()
+        {
+            return width * height * depth;
+        }
+
+        public Boolean Volume3()
+        {
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1() //volume true calc
+        {
+            int a = 5, b = 3, c = 10;
+
+            Box bb = new Box(a,b,c);
+            int v = bb.Volume2();
+
+            Assert.AreEqual(a * b * c, v);
+        }
+
+        [TestMethod]
+        public void TestMethod2() //box not equal?
+        {
+            int a1 = 5, b1 = 3, c1 = 10;
+            Box box1 = new Box(a1, b1, c1);
+
+            int a2 = 6, b2 = 4, c2 = 11;
+            Box box2 = new Box(a2, b2, c2);
+
+            Assert.AreNotEqual(box1, box2);
+        }
+
+        [TestMethod]
+        public void TestMethod3()  //true?
+        {
+            int a = 5, b = 3, c = 10;
+
+            Box bb = new Box(a, b, c);
+
+            Assert.IsTrue(bb.Volume3());
+        }
+
+        [TestMethod]
+        
+        public void TestMethod4()  //wtf?)
+        {
+            int a = -2, b = 3, c = 10;
+
+            try { new Box(a, b, c); Assert.Fail(); }
+            catch ( BoxException e ) {}
+        }
+
+        [TestMethod]
+        public void TestMethod5() //fatbox
+        {
+            int a1 = 5, b1 = 3, c1 = 10;
+            Box box1 = new Box(a1, b1, c1);
+
+            int a2 = 6, b2 = 4, c2 = 11;
+            Box box2 = new Box(a2, b2, c2);
+
+            Assert.IsTrue(box1.Width<box2.Width);
+        }
+    }
+}
